b0348623fcdd591f119f672ea23590fcfefba4e9
masudbarki <61346065+masudbaki@users.noreply.github.com>
2020-12-23 14:07:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def process_image():
         if 'file' in request.files:
             file = request.files['file']
             if allowed_file(file.filename):
-                #file.save(input_path)
+                file.save(input_path)
                 print(os.getcwd())
             try:
                 render_factor = request.form.getlist('render_factor')[0]
